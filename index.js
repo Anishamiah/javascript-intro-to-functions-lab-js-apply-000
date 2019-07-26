8bda07cc7d88696d4ad68a1ae913b9a392f6732b
Anisha Miah <anishamiah@gmail.com>
2019-07-26 07:01:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   var lowercase = "hello"
   if(string === lowercase) {
-    return "I can\'t hear you\!"
+    return "I can't hear you!"
   }
 }
 
 function sayHiToGrandma(string){
   var uppercase = "HELLO"
   if(string === uppercase){
-    return "YES INDEED\!"
+    return "YES INDEED!"
   }
 }
 
